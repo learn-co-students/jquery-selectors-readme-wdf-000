4fa014357fc5fec95555b425bb91df6a271c2a47
ozPop <ozziepopovas@gmail.com>
2016-10-17 17:05:25 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+$(document).ready(function() {
+  var i = $('.pics #pic-list img:first-child');
+  console.log(i[0]);
+});
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
